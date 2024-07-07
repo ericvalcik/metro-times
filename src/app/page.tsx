05553c29dd9a6e02694ecf7f7b5c59df16779087
eric valcik <eric.valcik@gmail.com>
2024-07-08 00:08:46 +0200

    make items in combobox clickable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ComboBoxResponsive } from "@/components/ui/combobox";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen w-full flex-col items-start justify-between pt-12">
       <ComboBoxResponsive />
     </main>
   );
