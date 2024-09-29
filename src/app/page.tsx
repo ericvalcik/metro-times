5ebbd4cd0de14af16644961d23530e876e10fda8
eric valcik <eric.valcik@gmail.com>
2024-09-29 21:57:36 +0200

    add location getting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import { ComboBoxResponsive } from "@/components/ui/combobox";
 import { AppContextProvider } from "@/components/AppContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,13 +11,37 @@ import { TypographyH1 } from "@/components/ui/typography";
 // Create a client
 const queryClient = new QueryClient();
 
+type Coords = [number, number];
+
 export default function Home() {
+  const [coords, setCoords] = useState<Coords | null>(null);
+
+  const onSuccess = (position: GeolocationPosition) => {
+    setCoords([position.coords.latitude, position.coords.longitude]);
+  };
+
+  const onError = (error: GeolocationPositionError) => {
+    console.error(error);
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      console.log(
+        "Geolocation is supported by this browser. Getting location...",
+      );
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
         <main className="px-4">
           <div className="max-w-[358px] mx-auto flex min-h-screen w-full flex-col items-start pt-12">
             <TypographyH1>Precise Metro Departures 🎯</TypographyH1>
+            <pre>{JSON.stringify(coords, null, 2)}</pre>
             <div className="pb-4">
               <ComboBoxResponsive />
             </div>
